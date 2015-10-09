5bb245f9efd954eabe0c47043eb490c54febc1ff
Andrey Bogdanov <ololoepepe@gmail.com>
2015-10-09 20:59:53 +0500

    Implement readonly and read/write locks differentiation

--- README.md
+++ README.md
@@ -19,20 +19,20 @@ How does this locking module work?
 ==================================
 
 First we create a class (Lock) with the promised-mongo MongoDB database instance being bound to it.
+
 Then we create a new lock object with some key (for example, if we lock a file, the key may be the name of that file).
+
 After that we call the .lock() method of the lock object which returns a promise resolving when the lock is acquired.
-When we finish our work, we call .unlock the method of the lock object which also returns a promise resolving when unlocking is done.
+
+When we finish our work, we call the .unlock method of the lock object which also returns a promise resolving when unlocking is done.
+
 When a lock is acquired by a lock object, other locks with the same key will wait until the first object releases the lock.
-Pending locks are added to the queue (UUIDs are used to identify the objects).
 
-How does this work under the hood?
-==================================
+Pending locks are added to the queue (UUIDs are used to identify the objects). Read/write locks has higher priority than readonly ones.
 
-First the lock object tries to find an object with the specified key in the collection "_pmongoLocks".
-If it does not find one, than it creates it. If another process creates a lock with that key before, then our object catches the error and tries to find the object in the collection again.
-When an object is found, the lock checks if it is in an unlocked state and if the first entry in the queue matches the lock's id. If so, it changes the state to locked and removes the first id from the queue.
-Otherwise, the lock waits for retryDelay milliseconds (10 by default) and then repeats the above step.
-When .unlock() method is called, the lock finds an object with the corresponding id in the collection and sets it's state to unlocked.
+There are two locking modes: readonly and read/write. A lock may be acquired by one or more readonly lockers or by a single read/write locker at the same time, exclusively.
+
+Use Lock.ReadOnly or Lock.ReadWrite to specify locking mode. If not specified, locking mode defaults to read/write.
 
 Let's review Configuration
 ==========================
@@ -45,8 +45,3 @@ options.retryDelay
 ------------------
 
 retryDelay is the number of milliseconds to wait before retrying to acquire the lock.
-
-TODO
-====
-
-1. Implement readonly and read-write locks differentiation.
--- index.js
+++ index.js
@@ -1,5 +1,21 @@
 var uuid = require("uuid");
 
+var queueContainsLock = function(queue, id) {
+    for (var i = 0; i < queue.length; ++i) {
+        if (queue[i].id = id)
+            return true;
+    }
+    return false;
+};
+
+var lockIsNextInQueue = function(queue, id, mode) {
+    for (var i = 0; i < queue.length; ++i) {
+        if (queue[i].id = id && (queue[i].mode == mode || mode == "READ_WRITE"))
+            return true;
+    }
+    return false;
+};
+
 module.exports = function(db) {
     if (!db || !db.__proto__ || !db.__proto__.constructor || db.__proto__.constructor.name != "Database"
         || !db.collection) { //NOTE: This is a very rough check
@@ -36,8 +52,11 @@ module.exports = function(db) {
                 return collection.insert([{
                     _id: _this.key,
                     mode: Lock.NotLocked,
-                    queue: [_this.id],
-                    current: null
+                    queue: [{
+                        id: _this.id,
+                        mode: _this.mode
+                    }],
+                    current: []
                 }]).catch(function(err) {
                     //NOTE: If a lock was inserted by someone else while we were finding it, try to find it again
                     return collection.findOne({ _id: _this.key }).then(function(lock) {
@@ -48,10 +67,14 @@ module.exports = function(db) {
                 if (!locks || !locks[0])
                     return Promise.reject("Unable to create lock");
                 var lock = locks[0];
-                if (lock.mode != Lock.NotLocked || lock.queue[0] != _this.id) {
+                if (lock.mode == Lock.ReadWrite || (_this.mode == Lock.ReadWrite && lock.mode != Lock.NotLocked) ||
+                    !lockIsNextInQueue(lock.queue, _this.id, _this.mode)) {
                     var p;
-                    if (lock.queue.indexOf(_this.id) < 0) {
-                        lock.queue.push(_this.id);
+                    if (!queueContainsLock(lock.queue, _this.id)) {
+                        lock.queue.push({
+                            id: _this.id,
+                            mode: _this.mode
+                        });
                         p = collection.update({ _id: _this.key }, lock);
                     } else {
                         p = Promise.resolve();
@@ -66,7 +89,7 @@ module.exports = function(db) {
                 }
                 lock.queue.splice(0, 1);
                 lock.mode = _this.mode;
-                lock.current = _this.id;
+                lock.current.push(_this.id);
                 _this.locking = false;
                 _this.acquired = true;
                 return collection.update({ _id: _this.key }, lock).then(function() {
@@ -87,10 +110,12 @@ module.exports = function(db) {
             return collection.findOne({ _id: _this.key }).then(function(lock) {
                 if (!lock)
                     return Promise.reject("Unable to find lock");
-                if (lock.current != _this.id || lock.mode == Lock.NotAcquired)
+                var ind = lock.current.indexOf(_this.id);
+                if (ind < 0 || lock.mode == Lock.NotLocked)
                     return Promise.reject("Lock is not acquired");
-                lock.mode = Lock.NotLocked;
-                lock.current = null;
+                lock.current.splice(ind, 1);
+                if (lock.current.length < 1)
+                    lock.mode = Lock.NotLocked;
                 _this.unlocking = false;
                 _this.acquired = false;
                 var p;
